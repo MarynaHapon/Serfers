ed7a26e3a30f3fc962f4a56d2974e9a9ea833804
Maryna Hapon <marynahapon@gmail.com>
2017-08-08 17:47:06 +0300

    carousel
    module surfboards

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
     $('#fullpage').fullpage();
 });
+
+    // caroucel
+// asNavFor can be set a selector string
+asNavFor: '.carousel-main'
+// or an element
+asNavFor: $('.carousel-main')[0]
+asNavFor: document.querySelector('.carousel-main')
+
+$('.carousel-main').flickity();
+// 2nd carousel, navigation
+$('.carousel-nav').flickity({
+    asNavFor: '.carousel-main',
+    contain: true,
+    pageDots: false
+});
