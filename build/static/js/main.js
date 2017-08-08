176c454ed1fd42305099fd0ab97ddb8fb1f0b2c1
Maryna Hapon <marynahapon@gmail.com>
2017-08-08 21:51:25 +0300

    component radio
    ratings with radio buttons

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -2,6 +2,25 @@ $(document).ready(function() {
     $('#fullpage').fullpage();
 });
 
+    // surfboards list
+// external js: flickity.pkgd.js
+
+var $surfboardList = $('.surfboard-list').flickity({
+    draggable: false,
+    prevNextButtons: false,
+    pageDots: false
+});
+
+// previous
+$('.surfboard-btn--previous').on( 'click', function() {
+    $surfboardList.flickity('previous', true);
+});
+// next
+$('.surfboard-btn--next').on( 'click', function() {
+    $surfboardList.flickity( 'next', true );
+});
+
+
     // caroucel
 // asNavFor can be set a selector string
 asNavFor: '.carousel-main'
@@ -15,4 +34,14 @@ $('.carousel-nav').flickity({
     asNavFor: '.carousel-main',
     contain: true,
     pageDots: false
+});
+
+
+    //  radio
+var logID = 'log',
+    log = $('<div id="'+logID+'"></div>');
+$('body').append(log);
+$('[type*="radio"]').change(function () {
+    var me = $(this);
+    log.html(me.attr('value'));
 });
